a4b0c76c3e27e2d325a17f42bb99075b18008d9a
Onixs50 <105543843+Onixs50@users.noreply.github.com>
2024-07-26 21:36:48 +0300

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Variables
+
+# Check if another instance of the script is running
+if pgrep -f "fix.sh" > /dev/null 2>&1; then
+    echo "Another instance of the script is running."
+    exit 1
+fi
+#!/bin/bash
+
 service_name="stationd"
 error_strings=(
   "ERROR"
@@ -29,8 +36,6 @@ restart_delay=120
 config_file="$HOME/.tracks/config/sequencer.toml"
 repository_path="$HOME/fix-stationd-errors"
 update_flag="$repository_path/update_flag.txt"
-script_name=$(basename "$0")
-lockfile="/tmp/${script_name}.lock"
 
 unique_urls=(
   "https://t-airchains.rpc.utsa.tech/"
@@ -84,90 +89,89 @@ unique_urls=(
   "https://test-rpc.airchain.com/"
 )
 
-function acquire_lock {
-    exec 200>$lockfile
-    flock -n 200 || { echo -e "\e[31mAnother instance of the script is running.\e[0m"; exit 1; }
-}
-
-function release_lock {
-    flock -u 200
-    rm -f "$lockfile"
-}
-
 function select_random_url {
-    local array=("$@")
-    local rand_index=$(( RANDOM % ${#array[@]} ))
-    echo "${array[$rand_index]}"
+  local array=("$@")
+  local rand_index=$(( RANDOM % ${#array[@]} ))
+  echo "${array[$rand_index]}"
 }
 
 function update_rpc_and_restart {
-    local random_url=$(select_random_url "${unique_urls[@]}")
-    sed -i -e "s|JunctionRPC = \"[^\"]*\"|JunctionRPC = \"$random_url\"|" "$config_file"
-    if [[ $? -ne 0 ]]; then
-        echo -e "\e[31mFailed to update RPC URL in config file.\e[0m"
-        exit 1
-    fi
+  local random_url=$(select_random_url "${unique_urls[@]}")
+  sed -i -e "s|JunctionRPC = \"[^\"]*\"|JunctionRPC = \"$random_url\"|" "$config_file"
+  if [[ $? -ne 0 ]]; then
+    echo -e "\e[31mFailed to update RPC URL in config file.\e[0m"
+    exit 1
+  fi
 
-    echo -e "\e[32mService $service_name stopped.\e[0m"
-    systemctl stop "$service_name"
-    if [[ $? -ne 0 ]]; then
-        echo -e "\e[31mFailed to stop service $service_name.\e[0m"
-        exit 1
-    fi
+  echo -e "\e[32mService $service_name stopped.\e[0m"
+  systemctl stop "$service_name"
+  if [[ $? -ne 0 ]]; then
+    echo -e "\e[31mFailed to stop service $service_name.\e[0m"
+    exit 1
+  fi
 
-    echo -e "\e[32mUpdating repository...\e[0m"
-    cd ~/tracks || exit
-    git pull || { echo -e "\e[31mFailed to update repository.\e[0m"; exit 1; }
+  echo -e "\e[32mUpdating repository...\e[0m"
+  cd "$repository_path" || exit
+  git fetch --quiet
+  git reset --hard HEAD
+  git pull --quiet
+  if [[ $? -ne 0 ]]; then
+    echo -e "\e[31mFailed to update repository.\e[0m"
+    exit 1
+  fi
 
-    echo -e "\e[32mRPC URL updated to: $random_url\e[0m"
-    echo -e "\e[32mPerforming rollback...\e[0m"
-    for i in {1..3}; do
-        go run cmd/main.go rollback || exit
-    done
-    echo -e "\e[32mRollback completed.\e[0m"
+  echo -e "\e[32mRPC URL updated to: $random_url\e[0m"
+  echo -e "\e[32mPerforming rollback...\e[0m"
+  cd ~/tracks || exit
+  go run cmd/main.go rollback || exit
+  go run cmd/main.go rollback || exit
+  go run cmd/main.go rollback || exit
+  echo -e "\e[32mRollback completed.\e[0m"
 
-    echo -e "\e[32mRestarting service $service_name...\e[0m"
-    systemctl start "$service_name"
-    if [[ $? -ne 0 ]]; then
-        echo -e "\e[31mFailed to restart service $service_name.\e[0m"
-        exit 1
-    fi
-    echo -e "\e[32mService $service_name restarted successfully!\e[0m"
-    sleep "$restart_delay"
+  echo -e "\e[32mRestarting service $service_name...\e[0m"
+  systemctl start "$service_name"
+  if [[ $? -ne 0 ]]; then
+    echo -e "\e[31mFailed to restart service $service_name.\e[0m"
+    exit 1
+  fi
+  echo -e "\e[32mService $service_name restarted successfully!\e[0m"
+  sleep "$restart_delay"
 }
 
 function display_waiting_message {
-    echo -e "\e[35mI am waiting for you AIRCHAIN...\e[0m"
+  echo -e "\e[35mI am waiting for you AIRCHAIN...\e[0m"
 }
 
 function check_for_updates {
-    cd "$repository_path" || exit
+  cd "$repository_path" || exit
 
-    git fetch --quiet
+  git fetch --quiet
 
-    local local_commit=$(git rev-parse @)
-    local remote_commit=$(git rev-parse @{u})
+  local local_commit=$(git rev-parse @)
+  local remote_commit=$(git rev-parse @{u})
 
-    if [ "$local_commit" != "$remote_commit" ]; then
-        echo -e "\e[31m+\e[0m \e[32m+\e[0m \e[31m+\e[0m \e[32m+\e[0m \e[31m+\e[0m"
-        echo -e "\e[32mUpdate found. Downloading and updating...\e[0m"
-        wget -q https://raw.githubusercontent.com/Onixs50/fix-stationd-errors/main/fix.sh -O "$repository_path/fix.sh" > /dev/null 2>&1
-        chmod +x "$repository_path/fix.sh" > /dev/null 2>&1
+  if [ "$local_commit" != "$remote_commit" ]; then
+    echo -e "\e[31m+\e[0m \e[32m+\e[0m \e[31m+\e[0m \e[32m+\e[0m \e[31m+\e[0m"
+    echo -e "\e[32mUpdate found. Downloading and updating...\e[0m"
+    
+    wget -q https://raw.githubusercontent.com/Onixs50/fix-stationd-errors/main/fix.sh -O fix.sh > /dev/null 2>&1
+    chmod +x fix.sh > /dev/null 2>&1
 
-        touch "$update_flag"
-        echo -e "\e[32mUpdate completed successfully!\e[0m"
-        echo -e "\e[32mStopping current script...\e[0m"
+    echo -e "\e[32mUpdate completed successfully!\e[0m"
 
-        # Run the updated script in the same screen session
-        "$repository_path/fix.sh"
-        exit 0
-    else
-        rm -f "$update_flag"
-    fi
+    touch "$update_flag"
+    echo -e "\e[32mStopping current script...\e[0m"
+    
+    # Stopping the current instance
+    pkill -f "fix.sh"
+
+    echo -e "\e[32mRestarting script to apply changes...\e[0m"
+    exec "$0"
+  else
+    rm -f "$update_flag"
+  fi
 }
 
-# Start the script
-acquire_lock
 echo -e "\e[1;32m============================================\e[0m"
 echo -e "\e[1;32m      Script Monitoring and Update Tool      \e[0m"
 echo -e "\e[1;32m          Created by Onixia                 \e[0m"
@@ -175,26 +179,20 @@ echo -e "\e[1;32m============================================\e[0m"
 echo -e "\e[1;36mScript started to monitor logs...\e[0m"
 echo "Timestamp: $(date)"
 echo -e "\e[32mCoded By Onixia\e[0m"
-echo " started  monitoring errors in shit chain..."
+echo "Script started to monitor errors in shit airchain..."
 echo "Timestamp: $(date)"
 
 while true; do
-    check_for_updates
+  check_for_updates
 
-    logs=$(systemctl status "$service_name" --no-pager | tail -n 10)
+  logs=$(systemctl status "$service_name" --no-pager | tail -n 100)
+  for error in "${error_strings[@]}"; do
+    if echo "$logs" | grep -q "$error"; then
+      echo -e "\e[31mFound error ('$error') in logs, updating $config_file and restarting $service_name...\e[0m"
+      update_rpc_and_restart
+      break
+    fi
+  done
 
-    for error_string in "${error_strings[@]}"; do
-        if echo "$logs" | grep -q "$error_string"; then
-            echo -e "\e[31mFound error ('$error_string') in logs, updating $config_file and restarting $service_name...\e[0m"
-
-            update_rpc_and_restart
-            display_waiting_message
-            break
-        fi
-    done
-
-    sleep 60  
+  sleep 60
 done
-
-release_lock
-echo -e "\e[32mCoded By Onixia\e[0m"
